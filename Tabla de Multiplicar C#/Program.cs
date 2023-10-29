1daa8e1e1af9a722ba85cba706cd123ce2c803d4
Amin070204 <145484915+Amin070204@users.noreply.github.com>
2023-10-29 09:38:50 -0600

    Agregar archivos de proyecto.

---
+++ Tabla de Multiplicar C#/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace TABLAS_DE_MULTIPLICACION_CON_WHILE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int NUM, I, RESUL;
+            string linea;
+            Console.Write("DIGITE NUMERO: "); linea = Console.ReadLine();
+            NUM = int.Parse(linea);
+            I = 1;
+            while ((I <= 12))
+            {
+                RESUL = NUM * I;
+                Console.WriteLine("{0} * {1} = {2}", NUM, I, RESUL);
+                I = I + 1;
+            }
+            Console.Write("Pulse una tecla: "); Console.ReadLine();
+        }
+    }
+}
